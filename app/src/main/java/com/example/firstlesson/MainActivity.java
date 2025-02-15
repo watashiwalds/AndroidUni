00a4b5549a44a5c7559aafa496b7c0810e8c1f2b
watashiwalds <143416825+watashiwalds@users.noreply.github.com>
2025-02-15 10:40:12 +0700

    add menu in main activity

--- app/src/main/java/com/example/firstlesson/MainActivity.java
+++ app/src/main/java/com/example/firstlesson/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.firstlesson;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -22,6 +29,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Show the Action Bar (if hidden)
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.show();
+        }
     }
 
     public void openBMI(View v) {
@@ -32,5 +45,27 @@ public class MainActivity extends AppCompatActivity {
     public void openListView(View v) {
         Intent it = new Intent(this, ListViewDemo.class);
         startActivity(it);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        TextView changethis = findViewById(R.id.tvChangeColor);
+        if (item.getItemId() == R.id.makeRed) {
+            changethis.setBackgroundColor(Color.RED);
+        }
+        if (item.getItemId() == R.id.makeGreen) {
+            changethis.setBackgroundColor(Color.GREEN);
+        }
+        if (item.getItemId() == R.id.makeBlue) {
+            changethis.setBackgroundColor(Color.BLUE);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
